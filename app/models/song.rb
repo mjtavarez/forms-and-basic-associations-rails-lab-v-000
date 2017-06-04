bc9722e6672e2d473f8773827a6de66f0bf8d39c
Miguel Tavarez <tavarezster@gmail.com>
2017-06-04 01:19:44 +0000

    Music player domain with hard-coded form fields for Notes (belong_to @song via has_many)

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,38 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+  
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+  
+  def note_contents=(notes_array)
+    notes_array.collect do |note|
+      if note.strip != "" && !self.notes.include?(Note.find_by(content: note))
+        self.notes.build(content: content)
+      end
+    end
+  end
+  
+  def note_contents
+    self.notes.collect{|note| note.content}
+  end
+  
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+  
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
 end
 
